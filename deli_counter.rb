dc756bb4785a2bbb5a0d82e022964df205262a84
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 21:00:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 # Write your code here.
-katz=Array.new
 def line(katz)
   if katz.size == 0
     "The line is currently empty."
   else
     string = "The line is currently:"
     katz.each_with_index do |person,i|
-      string = string+" #{i+1}. #{person}"
+      string = string + " #{i+1}. #{person}"
     end
     string
   end
 end
+
