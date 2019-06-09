0b3d2476969daae9fc6ececc76eec835cff04d28
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 21:00:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 def line(katz)
   if katz.size == 0
-    "The line is currently empty."
+    puts "The line is currently empty."
   else
     string = "The line is currently:"
     katz.each_with_index do |person,i|
       string = string + " #{i+1}. #{person}"
     end
-    string
+    puts string
   end
 end
